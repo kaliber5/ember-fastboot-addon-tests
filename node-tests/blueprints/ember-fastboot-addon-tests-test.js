33dc059c1dc9ea67727b106dacc0a966ca6e94b3
simonihmig <ihmig@kaliber5.de>
2017-01-25 00:21:05 +0100

    Added blueprint tests

---
+++ node-tests/blueprints/ember-fastboot-addon-tests-test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const blueprintHelpers = require('ember-cli-blueprint-test-helpers/helpers');
+const setupTestHooks = blueprintHelpers.setupTestHooks;
+const emberNew = blueprintHelpers.emberNew;
+const emberGenerateDestroy = blueprintHelpers.emberGenerateDestroy;
+
+const expect = require('ember-cli-blueprint-test-helpers/chai').expect;
+
+describe('Acceptance: ember generate and destroy ember-fastboot-addon-tests', function() {
+  setupTestHooks(this);
+
+  it('ember-fastboot-addon-tests foo', function() {
+    let args = ['ember-fastboot-addon-tests', 'foo'];
+
+    // pass any additional command line options in the arguments array
+    return emberNew()
+      .then(() => emberGenerateDestroy(args, (file) => {
+        expect(file('fastboot-tests/fixtures/fastboot/app/router.js'))
+          .to.contain('Router.map(function() {');
+
+        expect(file('fastboot-tests/fixtures/fastboot/app/templates/index.hbs'))
+          .to.contain('<h1>ember-fastboot-addon-tests</h1>');
+    }));
+  });
+});
---
+++ node-tests/blueprints/fastboot-test-test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const blueprintHelpers = require('ember-cli-blueprint-test-helpers/helpers');
+const setupTestHooks = blueprintHelpers.setupTestHooks;
+const emberNew = blueprintHelpers.emberNew;
+const emberGenerate = blueprintHelpers.emberGenerate;
+const emberDestroy = blueprintHelpers.emberDestroy;
+
+const chai = require('ember-cli-blueprint-test-helpers/chai');
+const file = chai.file;
+const expect = chai.expect;
+
+const defaultBlueprintArgs = ['ember-fastboot-addon-tests', 'dummy'];
+
+describe('Acceptance: ember generate and destroy fastboot-test', function() {
+  setupTestHooks(this);
+
+  it('fastboot-test foo', function() {
+    let args = ['fastboot-test', 'foo'];
+
+    return emberNew()
+      .then(() => emberGenerate(defaultBlueprintArgs))
+      .then(() => emberGenerate(args))
+      .then(() => {
+        expect(file('fastboot-tests/foo-test.js'))
+          .to.contain('return this.visit(\'/foo\')');
+
+        expect(file('fastboot-tests/fixtures/fastboot/app/router.js'))
+          .to.contain('this.route(\'foo\')');
+      })
+      .then(() => emberDestroy(args))
+      .then(() => emberDestroy(defaultBlueprintArgs));
+  });
+});
