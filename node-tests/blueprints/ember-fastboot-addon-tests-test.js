d9c37c51845f896772f7d3842216a1b374ed1524
simon <ihmig@kaliber5.de>
2017-02-09 18:48:46 +0100

    Extended default blueprint test

--- node-tests/blueprints/ember-fastboot-addon-tests-test.js
+++ node-tests/blueprints/ember-fastboot-addon-tests-test.js
@@ -10,7 +10,7 @@ const expect = require('ember-cli-blueprint-test-helpers/chai').expect;
 describe('Acceptance: ember generate and destroy ember-fastboot-addon-tests', function() {
   setupTestHooks(this);
 
-  it('ember-fastboot-addon-tests foo', function() {
+  it('ember-fastboot-addon-tests', function() {
     let args = ['ember-fastboot-addon-tests', 'foo'];
 
     // pass any additional command line options in the arguments array
@@ -18,6 +18,9 @@ describe('Acceptance: ember generate and destroy ember-fastboot-addon-tests', fu
       .then(() => emberGenerateDestroy(args, (file) => {
         expect(file('fastboot-tests/fixtures/fastboot/app/router.js'))
           .to.contain('Router.map(function() {');
+
+        expect(file('fastboot-tests/index-test.js'))
+          .to.contain('return this.visit(\'/\')');
 
         expect(file('fastboot-tests/fixtures/fastboot/app/templates/index.hbs'))
           .to.contain('<h1>ember-fastboot-addon-tests</h1>');
