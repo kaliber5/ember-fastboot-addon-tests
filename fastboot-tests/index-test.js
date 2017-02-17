2eabb967f982dc7e2c292120be73afc5efdf1e20
simonihmig <ihmig@kaliber5.de>
2017-02-18 00:15:12 +0100

    Added own fastboot acceptance test

---
+++ fastboot-tests/index-test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('index', function() {
+
+  it('renders', function() {
+    return this.visit('/')
+      .then(function(res) {
+        let $ = res.jQuery;
+        let response = res.response;
+
+        expect(response.statusCode).to.equal(200);
+        expect($('body').length).to.equal(1);
+        expect($('h1').text().trim()).to.equal('ember-fastboot-addon-tests');
+      });
+  });
+
+});
